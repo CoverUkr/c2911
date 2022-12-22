ddcf10c89184d0fb9b1e2445012d3e95dcf5c342
ASRock <dima.dumaretsky@gmail.com>
2022-12-22 18:01:37 +0200

    inition commit

---
+++ Vp.py
@@ -0,0 +1,12 @@
+#class Student:
+    #def __init__(self):
+        #self.height=170
+#student=Student()
+#print(nick.height)
+
+
+class Rec:
+    def area(self, a, b):
+        return a * b
+rect=Rec()
+print(rect.area(4,5))
